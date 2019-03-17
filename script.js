75613bf953788bddea3995431b64e81e171eec8b
andreyborovkov <andrey_borovkov@yahoo.com>
2019-03-17 13:54:32 -0400

    projects section moved, index page updated

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ addNavEventListeners();
 addArticleEventListeners();
 addWindowEventListeners();
 addModalEventListeners();
-addSliderEventListeners();
  
  
  
@@ -82,50 +81,4 @@ addSliderEventListeners();
     }
 
 
-     // Image Carousel / Slider
-  function addSliderEventListeners() {
-
-    const sliderPrev = document.getElementById('slider-prev');
-    const sliderNext = document.getElementById('slider-next');
-    const sliderFrame = document.getElementById('slider-frame');
-
-    const slides =
-      Array.from(sliderFrame.querySelectorAll('div[data-src]'))
-        .map(div => {
-          return {
-            imageUrl: div.getAttribute('data-src'),
-          };
-        });
-
-    let sliderIndex = 0;
-    displaySliderImage();
-
-    function displaySliderImage() {
-      let { imageUrl, caption } = slides[sliderIndex];
-      sliderFrame.style.backgroundImage = `url('${imageUrl}')`;
     
-    }
-
-    function displayPrevSliderImage() {
-      sliderIndex--;
-      if (sliderIndex < 0) {
-        sliderIndex = slides.length - 1;
-      }
-      displaySliderImage();
-    }
-
-    function displayNextSliderImage() {
-      sliderIndex++;
-      if (sliderIndex === slides.length) {
-        sliderIndex = 0;
-      }
-      displaySliderImage();
-    }
-
-    sliderPrev.addEventListener('click', displayPrevSliderImage);
-    sliderNext.addEventListener('click', displayNextSliderImage);
-
-    setInterval(function() {
-      displayNextSliderImage();
-    }, 4000);
-  }
